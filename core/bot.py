86617a259d8fa6bfcd2db547b92e3b86a4c4b707
ldfxxx404 <limur9990@gmail.com>
2025-11-14 21:03:04 +0300

    refactor: rename message handling functions for consistency and clarity; migrate template messages to utils

--- core/bot.py
+++ core/bot.py
@@ -1,11 +1,11 @@
 from utils.loader import dp
 from random import choice
 import asyncio
-from templateMessages import (
-    replyMessages,
-    startMessages,
-    askMessages,
-    replyToAskMessage,
+from utils.templateMessages import (
+    reply_to_ask_messages,
+    reply_messages,
+    start_messages,
+    ask_messages
 )
 
 from aiogram.filters import CommandStart
@@ -14,19 +14,19 @@ from aiogram.enums import ChatAction
 
 
 @dp.message(CommandStart())
-async def startHandler(message: Message) -> None:
-    await message.answer(choice(startMessages))
+async def start_handler(message: Message) -> None:
+    await message.answer(choice(start_messages))
 
 
 @dp.message()
-async def generalHandler(message: Message) -> None:
-    if message.text.lower() in [msg.lower() for msg in askMessages]:
-        await message.reply(choice(replyToAskMessage))
+async def general_handler(message: Message) -> None:
+    if message.text.lower() in [msg.lower() for msg in ask_messages]:
+        await message.reply(choice(reply_to_ask_messages))
         return
 
     await message.bot.send_chat_action(message.chat.id, ChatAction.TYPING)
     await asyncio.sleep(2.5)
-    await message.reply(choice(replyMessages))
+    await message.reply(choice(reply_messages))
     return
 
 
