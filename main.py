bdd85df76aa07c76a35fdca361451696dd44f2d6
lnkssr <gifzoner5@gmail.com>
2025-11-16 10:34:46 +0300

    chore: add linter

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from loader import dp
-import bot 
 from config import TOKEN
+from loader import dp
+import bot
 
 
 async def main() -> None:
-    bot_instance = Bot(token=TOKEN,
-                       default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot_instance = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot_instance)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
