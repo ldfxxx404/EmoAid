9f7b16841bda231fdb738ee4605b8fb7d987637f
ldfxxx404 <limur9990@gmail.com>
2025-11-16 17:04:12 +0300

    chore: add new picture

--- utils/images.py
+++ utils/images.py
@@ -4,6 +4,8 @@ pictures_url = [
     "https://img1.akspic.ru/crops/5/7/0/7/47075/47075-lavanda-video-purpur-pole-tsvetok-2560x1440.jpg",
     "https://img.freepik.com/free-photo/sunset-water_23-2151989512.jpg",
     "https://img.freepik.com/free-photo/violet-sunset_1361-153.jpg",
+    "https://wallpapers.com/images/hd/lime-green-aesthetic-2880-x-1800-oigtaysh0gk0bjer.jpg",
+    
 ]
 
 
